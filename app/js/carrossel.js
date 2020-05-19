de2e63379a656fab96c897e9ecefc553327c0036
Matheus Fonseca <matheus97sm@gmail.com>
2020-05-19 18:44:40 -0300

    Correção carrossel para touch

--- app/js/carrossel.js
+++ app/js/carrossel.js
@@ -11,64 +11,83 @@ export default function carrossel(carrosselSelector, carrosselWrapperSelector) {
 
   if (!carrosselEquipe || !carrosselWrapper) return null;
 
-  carrosselWrapper.addEventListener('mousedown', (e) => {
-    e.preventDefault();
+  const downEvents = ['mousedown', 'touchstart'];
+  const moveEvents = ['mousemove', 'touchmove'];
+  const upEvents = ['mouseup', 'touchend'];
 
-    cliqueInicial = e.clientX;
+  downEvents.forEach((event) => {
+    carrosselWrapper.addEventListener(event, (e) => {
+      if (e.type === 'touchstart') {
+        cliqueInicial = e.touches[0].clientX;
+      } else {
+        cliqueInicial = e.clientX;
+      }
 
-    carrosselWrapper.addEventListener('mousemove', moverCarrossel);
+      moveEvents.forEach((moveevent) => {
+        carrosselWrapper.addEventListener(moveevent, moverCarrossel);
+      });
+    });
   });
 
-  carrosselWrapper.addEventListener('mouseup', (e) => {
-    e.preventDefault();
+  upEvents.forEach((event) => {
+    carrosselWrapper.addEventListener(event, (e) => {
+      console.log(e);
+      if (e.type === 'touchend') {
+        cliqueFinal = e.changedTouches[0].clientX;
+      } else {
+        cliqueFinal = e.clientX;
+      }
 
-    cliqueFinal = e.clientX;
+      console.log(cliqueFinal);
 
-    if (
-      funcionarioAtivo < carrosselEquipe.length - 1 &&
-      cliqueInicial > cliqueFinal
-    ) {
-      funcionarioAtivo++;
+      if (
+        funcionarioAtivo < carrosselEquipe.length - 1 &&
+        cliqueInicial > cliqueFinal
+      ) {
+        funcionarioAtivo++;
 
-      carrosselEquipe.forEach((funcionario, index) => {
-        funcionario.classList.remove('active');
+        carrosselEquipe.forEach((funcionario, index) => {
+          funcionario.classList.remove('active');
 
-        if (index === funcionarioAtivo) funcionario.classList.add('active');
+          if (index === funcionarioAtivo) funcionario.classList.add('active');
+        });
+
+        carrosselPosition -= carrosselMovement;
+
+        carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
+        carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
+      } else if (funcionarioAtivo !== 0) {
+        funcionarioAtivo--;
+
+        carrosselEquipe.forEach((funcionario, index) => {
+          funcionario.classList.remove('active');
+
+          if (index === funcionarioAtivo) funcionario.classList.add('active');
+        });
+
+        carrosselPosition += carrosselMovement;
+
+        carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
+        carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
+      } else {
+        carrosselPosition = 0;
+
+        carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
+        carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
+      }
+
+      moveEvents.forEach((moveevent) => {
+        carrosselWrapper.removeEventListener(moveevent, moverCarrossel);
       });
-
-      carrosselPosition -= carrosselMovement;
-
-      carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
-      carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
-    } else if (funcionarioAtivo !== 0) {
-      funcionarioAtivo--;
-
-      carrosselEquipe.forEach((funcionario, index) => {
-        funcionario.classList.remove('active');
-
-        if (index === funcionarioAtivo) funcionario.classList.add('active');
-      });
-
-      carrosselPosition += carrosselMovement;
-
-      carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
-      carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
-    } else {
-      carrosselPosition = 0;
-
-      carrosselWrapper.style.setProperty('left', `${carrosselPosition}px`);
-      carrosselWrapper.style.setProperty('transform', 'translate3d(0, 0, 0)');
-    }
-
-    carrosselWrapper.removeEventListener('mousemove', moverCarrossel);
+    });
   });
 
   function moverCarrossel(e) {
-    e.preventDefault();
+    const move = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
 
     carrosselWrapper.style.setProperty(
       'transform',
-      `translate3d(${-(e, cliqueInicial - e.clientX)}px, 0, 0)`
+      `translate3d(${-(cliqueInicial - move)}px, 0, 0)`
     );
   }
 }
